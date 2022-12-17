da2f928e8ff22a8d8167b91025e27bad981b5d37
Krishna-Raj123 <110666553+Krishna-Raj123@users.noreply.github.com>
2022-12-17 10:15:56 +0530

    Add files via upload

---
+++ self_pro.py
@@ -0,0 +1,44 @@
+import time, socket, sys,os
+SEPARATOR = "<SEPARATOR>"
+print("\nWelcome to Chat Room\n")
+print("Initialising....\n")
+time.sleep(1)
+
+s = socket.socket()
+host = socket.gethostname()
+ip = socket.gethostbyname(host)
+port = 1234
+s.bind((host, port))
+print(host, "(", ip, ")\n")
+name = input(str("Enter your name: "))
+           
+s.listen(1)
+print("\nWaiting for incoming connections...\n")
+conn, addr = s.accept()
+print("Received connection from ", addr[0], "(", addr[1], ")\n")
+
+s_name = conn.recv(1024)
+s_name = s_name.decode()
+print(s_name, "has connected to the chat room\nEnter [e] to exit chat room and share file\n")
+conn.send(name.encode())
+
+
+while True:
+    message = input(str("Me : "))
+    if message == "[e]":
+        message = "Left chat room!"
+        conn.send(message.encode())
+        print("\n")
+        break
+    conn.send(message.encode())
+    message = conn.recv(1024)
+    message = message.decode()
+    print(s_name, ":", message)
+filename = input(str("please inter filename: "))
+file = open(filename , 'rb')
+file_data = file.read(1024)
+conn.send(file_data)
+print("data has been transmitted sucessfully")
+
+    
+   
